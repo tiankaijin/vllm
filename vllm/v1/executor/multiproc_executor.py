77ce785641bb14dc571d408d95cd98aea5b2f523
tiankaijin <tiankaijin1@h-partners.com>
2026-08-25 14:31:37 +0800

    adapt tp > 1 and reuse request

--- vllm/v1/executor/multiproc_executor.py
+++ vllm/v1/executor/multiproc_executor.py
@@ -425,6 +425,7 @@ class MultiprocExecutor(Executor):
 
         def get_response():
             responses = []
+            failure_msg = None
             for mq in response_mqs:
                 dequeue_timeout = (
                     None if deadline is None else max(0.0, deadline - time.monotonic())
@@ -432,13 +433,18 @@ class MultiprocExecutor(Executor):
                 try:
                     status, result = mq.dequeue(timeout=dequeue_timeout)
                 except TimeoutError as e:
-                    raise TimeoutError(f"RPC call to {method} timed out.") from e
+                    failure_msg = result
+                    if not self.parallel_config.enable_fault_tolerance or self.is_failed:
+                        break
+                    logger.info(f"drain response queue for other mq")
                 if status != WorkerProc.ResponseStatus.SUCCESS:
                     raise RuntimeError(
                         f"Worker failed with error '{result}', please check the"
                         " stack trace above for the root cause"
                     )
                 responses.append(result)
+            if failure_msg is not None:
+                raise RuntimeError(failure_msg)
             return responses[0] if output_rank is not None else responses
 
         future = FutureWrapper(
--- vllm/v1/fault_tolerance/engine_core_sentinel.py
+++ vllm/v1/fault_tolerance/engine_core_sentinel.py
@@ -85,8 +85,20 @@ class EngineCoreSentinel:
         )
 
         engine = self.engine
-        aborted = engine.scheduler.finish_requests(None, RequestStatus.FINISHED_ABORTED)
-        engine._send_abort_outputs(aborted)
+        ft_config = self.parallel_config.fault_tolerance_config
+        if ft_config.resume_requests_after_recovery:
+            timestamp = time.monotonic()
+            while engine.scheduler.running:
+                request = engine.scheduler.running.pop()
+                engine.scheduler.preempt_request(request, timestamp)
+                request.async_tokens_to_discard = request.num_output_placeholders
+                request.num_output_placeholders = 0
+            engine.scheduler.prev_step_scheduled_req_ids.clear()
+        else:
+            aborted = engine.scheduler.finish_requests(
+                None, RequestStatus.FINISHED_ABORTED
+            )
+            engine._send_abort_outputs(aborted)
         if engine.batch_queue is not None:
             engine.batch_queue.clear()
         if (
